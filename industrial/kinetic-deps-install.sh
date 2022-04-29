e5c54dac710e0f7c6cb58edb2509231a53a4a43f
Mohammad Wasil <mwasil@outlook.co.id>
2022-04-29 09:51:47 +0200

    [industrial] removed industrial image as it has been set up in mir

--- industrial/kinetic-deps-install.sh
+++
@@ -1,5 +0,0 @@
-sudo pip install --no-cache-dir --ignore-installed enum34
-sudo pip install --no-cache-dir -U numpy==1.16.5
-sudo pip install --no-cache-dir -U tensorflow==1.14.0
-sudo pip install --no-cache-dir -U scikit-learn easydict joblib
-
