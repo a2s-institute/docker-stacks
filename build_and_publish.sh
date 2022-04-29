b186dec328cd5e307c9fd53941a37e904e4d19eb
Mohammad Wasil <mwasil@outlook.co.id>
2022-04-29 09:57:42 +0200

    [ci] added domestic build, rename build_and_depoy -> build_and_publish

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,4 +1,4 @@
-# Build and deploy images
+# Build and publish images
 # Copyright b-it-bots, Hochschule Bonn-Rhein-Sieg
 
 DEPLOYMENT=""
@@ -76,10 +76,9 @@ else
 fi
 
 echo "Container registry/owner = $CONTAINER_REG_OWNER"
-
 echo "Deployment: $DEPLOYMENT"
 
-function gpu_notebook {
+function build_and_publish_gpu_notebook {
   cd gpu-notebook
   GPU_NOTEBOOK_TAG=$CONTAINER_REG_OWNER/gpu-notebook:$CUDA_VERSION 
 
@@ -87,7 +86,12 @@ function gpu_notebook {
   bash generate_dockerfile.sh
 
   docker build -t $GPU_NOTEBOOK_TAG --build-arg ROOT_CONTAINER=nvidia/cuda:$CUDA_VERSION .build/ 
-  if docker run -it --rm -d -p 8880:8888 $GPU_NOTEBOOK_TAG ; then echo "$GPU_NOTEBOOK_TAG is running"; else echo "Failed to run $GPU_NOTEBOOK_TAG" && exit 1; fi
+  if docker run -it --rm -d -p 8880:8888 $GPU_NOTEBOOK_TAG;
+  then
+    echo "$GPU_NOTEBOOK_TAG is running";
+  else
+    echo "Failed to run $GPU_NOTEBOOK_TAG" && exit 1;
+  fi
 
   if [ "$PUBLISH" = "all" ]
   then
@@ -96,8 +100,79 @@ function gpu_notebook {
   else
     echo "None is published"
   fi
-
 }
 
-# build docker
-gpu_notebook
+function build_and_publish_base_image {
+  cd base
+  BASE_PORT=5000
+  for ROS_DIST in */;
+  do
+    ROS_DISTRO=${ROS_DIST%/}
+    BASE_IMAGE_TAG=$CONTAINER_REG_OWNER/bitbots-base:$ROS_DISTRO
+    echo "Building base image $BASE_IMAGE_TAG"
+    docker build -t $BASE_IMAGE_TAG $ROS_DISTRO
+
+    if docker run -it --rm -d -p $BASE_PORT:$BASE_PORT $BASE_IMAGE_TAG;
+    then
+      echo "$BASE_IMAGE_TAG is running";
+    else
+      echo "Failed to run $BASE_IMAGE_TAG" && exit 1;
+    fi
+
+    let "BASE_PORT+=1"
+  done
+
+  if [ "$PUBLISH" = "all" ]
+  then
+    for ROS_DIST in */;
+    do
+      ROS_DISTRO=${ROS_DIST%/}
+      BASE_IMAGE_TAG=$CONTAINER_REG_OWNER/bitbots-base:$ROS_DISTRO
+      echo "Publishing base image $BASE_IMAGE_TAG"
+      docker push $BASE_IMAGE_TAG
+  done
+  else
+    echo "No base image is published"
+  fi
+  cd ..
+}
+
+function build_and_publish_domestic_image {
+  cd domestic
+  BASE_PORT=6000
+  for ROS_DIST in */;
+  do
+    ROS_DISTRO=${ROS_DIST%/}
+    DOMESTIC_IMAGE_TAG=$CONTAINER_REG_OWNER/bitbots-domestic:$ROS_DISTRO-base
+    echo "Building image $DOMESTIC_IMAGE_TAG"
+    docker build -t $DOMESTIC_IMAGE_TAG $ROS_DISTRO
+
+    if docker run -it --rm -d -p $BASE_PORT:$BASE_PORT $DOMESTIC_IMAGE_TAG;
+    then
+      echo "$DOMESTIC_IMAGE_TAG is running";
+    else
+      echo "Failed to run $DOMESTIC_IMAGE_TAG" && exit 1;
+    fi
+
+    let "BASE_PORT+=1"
+  done
+
+  if [ "$PUBLISH" = "all" ]
+  then
+    for ROS_DIST in */;
+    do
+      ROS_DISTRO=${ROS_DIST%/}
+      DOMESTIC_IMAGE_TAG=$CONTAINER_REG_OWNER/bitbots-domestic:$ROS_DISTRO-base
+      echo "Publishing image $DOMESTIC_IMAGE_TAG"
+      docker push $DOMESTIC_IMAGE_TAG
+  done
+  else
+    echo "No domestic image is published"
+  fi
+  cd ..
+}
+
+# build and push docker image
+build_and_publish_gpu_notebook
+build_and_publish_base_image
+build_and_publish_domestic_image
